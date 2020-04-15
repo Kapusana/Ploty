875a5654c573bcdb255f399412dec17eb3dbc96f
Kapusana <jkapusan@our.ecu.edu.au>
2020-04-15 21:01:22 +0800

    Changed to scrollView to enable single activity use.

--- app/src/main/java/com/example/graphapp/MainActivity.java
+++ app/src/main/java/com/example/graphapp/MainActivity.java
@@ -13,12 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /***Create onClickListener when confirm plot -> get intents,plot graph
+        Button confirmPlot = (Button) findViewById(R.id.confirmPlot);
+        confirmPlot.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //getting intent
+                Intent plotIntent = getIntent();
+                /* Edit this to int form *********
+                String x1 = plotIntent.getStringExtra(editGraphActivity.EXTRA_X1);
+
+            }
+        }); */
     }
 
+    //When switching to scrollView remove the lines below
+
+
      //should be called when the user taps the edit graph button
-    public void editGraph(View view) {
-        Intent editIntent = new Intent(this, editGraphActivity.class);
-        Button edit = (Button) findViewById(R.id.edit);
-        startActivity(editIntent);
-    }
+//    public void editGraph(View view) {
+//        Intent editIntent = new Intent(this, editGraphActivity.class);
+//        Button edit = (Button) findViewById(R.id.edit);
+//        startActivity(editIntent);
+//    }
 }
